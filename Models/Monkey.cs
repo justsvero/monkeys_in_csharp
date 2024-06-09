69d1ef58980b488a05a6aa9b66d68c226cb9cbf8
Sven Röseler <sven@svero.dev>
2024-06-09 10:34:35 +0200

    Initial commit

---
+++ Models/Monkey.cs
@@ -0,0 +1,17 @@
+namespace Svero.HelloWorld.Models;
+
+public class Monkey {
+    public string? Name { get; set; }
+
+    public string? Location { get; set; }
+
+    public string? Details { get; set; }
+
+    public string? Image { get; set; }
+
+    public int Popilation { get; set; }
+
+    public double Latitude { get; set; }
+
+    public double Longitude { get; set; }
+}
---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Net.Http.Json;
+using Svero.HelloWorld.Models;
+
+namespace Svero.HelloWorld
+{
+    public class Program {
+        const string URL = "https://www.montemagno.com/monkeys.json";
+
+        public static async Task Main(string[] args) {
+            var client = new HttpClient();
+
+            try {
+            var response = await client.GetAsync(URL);
+            if (response.IsSuccessStatusCode) {
+                var monkeys = await response.Content.ReadFromJsonAsync<List<Monkey>>();
+                if (monkeys?.Count > 0) {
+                    Console.WriteLine($"Number of monkeys found: {monkeys.Count}");
+
+                    foreach (var monkey in monkeys)
+                    {
+                        Console.WriteLine($"=> Monkey: {monkey?.Name}");
+                    }
+                }
+            } else {
+                Console.Error.WriteLine($"The server has returned an unexpected status code: {response.StatusCode}");
+            }
+            } catch (Exception e) {
+                Console.Error.WriteLine($"An error occurred: {e.Message}");
+            }
+        }
+    }
+}
